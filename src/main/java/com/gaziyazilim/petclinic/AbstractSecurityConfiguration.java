92f0c6420a465e237f99f9eda885b7f1a43d7c01
Orhan Gazi Kılıç <orhngazi@gmail.com>
2019-09-17 01:48:46 +0300

    Authentication delete request

--- src/main/java/com/gaziyazilim/petclinic/AbstractSecurityConfiguration.java
+++ src/main/java/com/gaziyazilim/petclinic/AbstractSecurityConfiguration.java
@@ -1,7 +1,4 @@
 package com.gaziyazilim.petclinic;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public abstract class AbstractSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+public class AbstractSecurityConfiguration {
 }
--- src/main/java/com/gaziyazilim/petclinic/RestSecurityConfiguration.java
+++ src/main/java/com/gaziyazilim/petclinic/RestSecurityConfiguration.java
@@ -1,17 +1,4 @@
 package com.gaziyazilim.petclinic;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-@Configuration
-@Order(value = 1)
-public class RestSecurityConfiguration extends AbstractSecurityConfiguration {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests().antMatchers("/rest/**").access("hasRole('EDITOR')");
-        http.csrf().disable();
-        http.httpBasic();
-    }
+public class RestSecurityConfiguration {
 }
