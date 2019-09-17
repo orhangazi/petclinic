4179c2b8a943537ebdd8616fcbc915a4dcaa6847
Orhan Gazi Kılıç <orhngazi@gmail.com>
2019-09-18 01:17:33 +0300

    unittest with spring-boot-start-test

---
+++ src/test/java/com/gaziyazilim/petclinic/PetClinicIntegrationTests.java
@@ -0,0 +1,26 @@
+package com.gaziyazilim.petclinic;
+
+import com.gaziyazilim.petclinic.model.Owner;
+import com.gaziyazilim.petclinic.service.PetClinicService;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {"spring.profiles.active=dev"})
+public class PetClinicIntegrationTests {
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @Test
+    public void testFindOwners(){
+        List<Owner> owners = petClinicService.findOwners();
+        MatcherAssert.assertThat(owners.size(), Matchers.equalTo(10));
+    }
+}
